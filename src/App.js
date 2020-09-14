d1683d5baf30859a7d2a5d12e73fe2436903f39f
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-13 18:44:26 -0600

    added changeHandler and submitOrder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Link, Route} from 'react-router-dom';
 import Home from './Home';
-import Pizza from './Pizza'
+import Pizza from './Pizza';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>Let us make you your perfect π!</p>
           <Link to='/'>Home</Link>
           <Link to='/pizza'>Order</Link>
 
--- src/Pizza.js
+++ src/Pizza.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Pizza = () => {
+
+    const [order, setOrder] = useState({
+        name: "",
+        size: "",
+        mushrooms: false,
+        olives: false,
+        peppers: false,
+        onions: false,
+        special: ""
+    });
+
+    const [errors, setErrors] = useState({
+        name: "",
+        size: "",
+        mushrooms: "",
+        olives: "",
+        peppers: "",
+        onions: "",
+        special: ""
+    });
+
+    const changeHandler = (e) => {
+        e.persist();
+        let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setOrder({
+            ...order, [e.target.name]: value
+        });
+    };
+
+    const submitOrder = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div>
             <h1>Order your pizza!!!</h1>
-            <form>
+            <form onSubmit={submitOrder}>
                 <label htmlFor='name'>
                     Name
-                    <input name='name'></input>
+                    <input 
+                    name='name'
+                    id='name'
+                    onChange={changeHandler}
+                    value={order.name}
+                    ></input>
                 </label><br></br>
 
                 <label htmlFor='size'>
                     Size
-                    <select name='size'>
+                    <select  name='size'
+                    id='size'
+                    onChange={changeHandler}
+                    value={order.size}>
                         <option>
                             Small
                         </option>
@@ -31,30 +72,57 @@ const Pizza = () => {
                 
                 <label htmlFor='mushrooms'>
                     Mushrooms
-                    <input type='checkbox' name='mushrooms'></input>
+                    <input type='checkbox'
+                     name='mushrooms'
+                     id='mushrooms'
+                     onChange={changeHandler}
+                     value={order.mushrooms}
+                    ></input>
                 </label>
 
                 <label htmlFor='olives'>
                     Olives
-                    <input type='checkbox' name='olives'></input>
+                    <input type='checkbox' 
+                    name='olives'
+                    id='olives'
+                    onChange={changeHandler}
+                    value={order.olives}
+                    ></input>
                 </label>
 
                 <label htmlFor='peppers'>
                     Peppers
-                    <input type='checkbox' name='peppers'></input>
+                    <input type='checkbox' 
+                    name='peppers'
+                    id='peppers'
+                    onChange={changeHandler}
+                    value={order.peppers}
+                    ></input>
                 </label>
 
                 <label htmlFor='onions'>
                     Onions
-                    <input type='checkbox' name='onions'></input>
+                    <input type='checkbox' 
+                    name='onions'
+                    id='onions'
+                    onChange={changeHandler}
+                    value={order.onions}
+                    ></input>
                 </label><br></br>
 
                 <label htmlFor='instructions'>
                     Special instructions
-                    <input type='textarea' name='instructions'></input>
+                    <input type='textarea' 
+                    name='instructions'
+                    id='instructions'
+                    onChange={changeHandler}
+                    value={order.instructions}
+                    ></input>
                 </label>
 
-                <button name='submit'>Order</button>
+                <button name='submit'
+                    class='submit'
+                >Order</button>
 
             </form>
         </div>
