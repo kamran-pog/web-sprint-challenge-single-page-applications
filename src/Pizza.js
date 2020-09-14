d6aea58bcef85c43f9af6840eb85175ff0d3bb6c
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-13 18:03:34 -0600

    added input elements to pizza form

--- src/Pizza.js
+++ src/Pizza.js
@@ -4,6 +4,59 @@ const Pizza = () => {
     return (
         <div>
             <h1>Order your pizza!!!</h1>
+            <form>
+                <label htmlFor='name'>
+                    Name
+                    <input name='name'></input>
+                </label><br></br>
+
+                <label htmlFor='size'>
+                    Size
+                    <select name='size'>
+                        <option>
+                            Small
+                        </option>
+                        <option>
+                            Medium
+                        </option>
+                        <option>
+                           Large
+                        </option>
+                        <option>
+                            Ginormous
+                        </option>
+                </select><br></br>
+
+                </label>
+                
+                <label htmlFor='mushrooms'>
+                    Mushrooms
+                    <input type='checkbox' name='mushrooms'></input>
+                </label>
+
+                <label htmlFor='olives'>
+                    Olives
+                    <input type='checkbox' name='olives'></input>
+                </label>
+
+                <label htmlFor='peppers'>
+                    Peppers
+                    <input type='checkbox' name='peppers'></input>
+                </label>
+
+                <label htmlFor='onions'>
+                    Onions
+                    <input type='checkbox' name='onions'></input>
+                </label><br></br>
+
+                <label htmlFor='instructions'>
+                    Special instructions
+                    <input type='textarea' name='instructions'></input>
+                </label>
+
+                <button name='submit'>Order</button>
+
+            </form>
         </div>
     );
 };
